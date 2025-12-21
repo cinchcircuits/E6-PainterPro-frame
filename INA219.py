18cfbb870f7dc24cb2c89a4fdeeb73d0d6c31caf
Not Me <mgregg@cinchsircuits.com>
2025-12-21 13:35:37 -0800

    Altering example to return what I need

--- INA219.py
+++ INA219.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# Returns only the state of charge in a percentage
 import smbus
 import time
 
@@ -192,22 +194,19 @@ if __name__=='__main__':
 
     # Create an INA219 instance.
     ina219 = INA219(addr=0x43)
-    while True:
-        bus_voltage = ina219.getBusVoltage_V()             # voltage on V- (load side)
-        shunt_voltage = ina219.getShuntVoltage_mV() / 1000 # voltage between V+ and V- across the shunt
-        current = ina219.getCurrent_mA()                   # current in mA
-        power = ina219.getPower_W()                        # power in W
-        p = (bus_voltage - 3)/1.2*100
-        if(p > 100):p = 100
-        if(p < 0):p = 0
+    bus_voltage = ina219.getBusVoltage_V()             # voltage on V- (load side)
+    shunt_voltage = ina219.getShuntVoltage_mV() / 1000 # voltage between V+ and V- across the shunt
+    current = ina219.getCurrent_mA()                   # current in mA
+    power = ina219.getPower_W()                        # power in W
+    p = (bus_voltage - 3)/1.2*100
+    if(p > 100):p = 100
+    if(p < 0):p = 0
 
-        # INA219 measure bus voltage on the load side. So PSU voltage = bus_voltage + shunt_voltage
-        #print("PSU Voltage:   {:6.3f} V".format(bus_voltage + shunt_voltage))
-        #print("Shunt Voltage: {:9.6f} V".format(shunt_voltage))
-        print("Load Voltage:  {:6.3f} V".format(bus_voltage))
-        print("Current:       {:6.3f} A".format(current/1000))
-        print("Power:         {:6.3f} W".format(power))
-        print("Percent:       {:3.1f}%".format(p))
-        print("")
-
-        time.sleep(2)
+    # INA219 measure bus voltage on the load side. So PSU voltage = bus_voltage + shunt_voltage
+    #print("PSU Voltage:   {:6.3f} V".format(bus_voltage + shunt_voltage))
+    #print("Shunt Voltage: {:9.6f} V".format(shunt_voltage))
+    #print("Load Voltage:  {:6.3f} V".format(bus_voltage))
+    #print("Current:       {:6.3f} A".format(current/1000))
+    #print("Power:         {:6.3f} W".format(power))
+    #print("Percent:       {:3.1f}%".format(p))
+    print("{:3.1f}".format(p))
