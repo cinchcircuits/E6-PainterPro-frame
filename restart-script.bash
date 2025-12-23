78c67d426e95bf2a6de1f178b8191bfa88f88ec4
Not Me <mgregg@cinchsircuits.com>
2025-12-23 07:53:13 -0800

    make restart script log

--- restart-script.bash
+++ restart-script.bash
@@ -1,6 +1,9 @@
 #!/bin/sh
-/usr/bin/sleep 600
 if [ ! -f /dev/shm/noreboot ]; then
+	/usr/bin/sleep 600
+	/usr/bin/logger -i e-ink 'restarting'
 	/usr/sbin/shutdown -h now
+else
+	/usr/bin/logger -i e-ink 'noreboot exists, not restarting'
 fi
 
